d5dd56a7f01e1e24b77344cd384361c31145cb9a
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2024-02-29 09:55:49 +0100

    more updates

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -94,7 +94,7 @@ function getResults() {
      * Returns 0, if no dice has the given face value.<br/>
      * Pre: 1 <= value <= 6;
      */
-    
+
   
 
 
@@ -143,17 +143,46 @@ function getResults() {
      * Return points for large straight.<br/>
      * Return 0, if the dice aren't showing 2,3,4,5,6.
      */
-    
+    function largeStraightPoints() {
+        let frequency = frequency();
+        let haveLargeStraight = true;
+        let bigStraight = 0;
+        for (let i = 2; i <= 6; i++){
+            if (frequency[i] < 1){
+                haveLargeStraight = false;
+            }
+        }
+        if (haveLargeStraight){
+            bigStraight = 20;     // the sum of 2,3,4,5,6
+        }
+        return bigStraight;
+    }
 
     /**
      * Return points for chance (the sum of face values).
      */
     
     function chancePoints() {
-
+        let frequency = frequency();
+        let sum = 0;
+        for (let i = 1; i <= 6; i++){
+            sum += frequency[i] * i;
+        }
+        return sum;
     }
 
     /**
      * Return points for yatzy (50 points).<br/>
      * Return 0, if there aren't 5 dice with the same face value.
      */
+
+    function yatzyPoints() {
+        let frequency = frequency();
+        let yatzy = 0;
+        for (let i = 1; i <= 6; i++) {
+            if (frequency[i] == 5){
+                yatzy = 50;
+            }
+        }
+        return yatzy;
+    }
