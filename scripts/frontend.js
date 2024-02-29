9597fe5cfc3c42f1faded1705c8526e5f2a9c0e0
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2024-02-29 11:58:43 +0100

    updates

--- scripts/frontend.js
+++ scripts/frontend.js
@@ -2,14 +2,42 @@ import * as gameLogic from "./game-logic.js";
 
 
 
-console.log(gameLogic.dices);
-
-gameLogic.rollDice();
-let idk = gameLogic.frequency();
 
 
 
-let rollBtn = document.querySelector()
+
+let rollBtn = document.querySelector(".roll-button");
+
+rollBtn.addEventListener("click", rollButton);
+
+
+function rollButton() {
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    gameLogic.rollDice();
+
+    let diceHolders = [];
+
+    for (let i = 1; i < 6; i++) {
+        diceHolders[i] = document.getElementById(`dice-holder-${i}`);
+
+        
+        const setPermanentDiceValue = async (j) => {
+            let yeet = (Math.random() * 400)+200;
+            await delay(yeet);
+            diceHolders[j].src = `./assets/dice-animation/dice_animation${j}.gif`;
+            
+            let diceValue = gameLogic.dices[j-1].value;
+
+            await delay(2000);
+            diceHolders[j].src = `./assets/die_${diceValue}.png`;
+          };
+        setPermanentDiceValue(i);
+    }
+
+
+
+}
 /* RollFunction 
 
 Den skal tjekke om nogen af terningerne er låste,
