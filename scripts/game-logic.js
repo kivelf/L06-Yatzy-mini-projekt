91f29d9df3bd28a0cb8d48b06dd65804517af632
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2024-02-29 09:26:06 +0100

    game logic method descriptions

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -30,3 +30,90 @@ function resetThrowCount() {
      * Roll the 5 dice. Only roll dice that are not hold.<br/>
      * Note: holdStatus[index] is true, if die no. index is hold (for index in [0..4]).
      */
+
+
+ // -------------------------------------------------------------------------
+
+    /**
+     * Return all results possible with the current face values.<br/>
+     * The order of the results is the same as on the score board.<br/>
+     * Note: This is an optional method. Comment this method out,<br/>
+     * if you don't want use it.
+     */
+
+
+     // -------------------------------------------------------------------------
+
+    // Return an int[7] containing the frequency of face values.
+    // Frequency at index v is the number of dice with the face value v, 1 <= v <= 6.
+    // Index 0 is not used.
+    // Note: This method can be used in several of the following methods.
+
+
+    /**
+     * Return same-value points for the given face value.<br/>
+     * Returns 0, if no dice has the given face value.<br/>
+     * Pre: 1 <= value <= 6;
+     */
+  
+
+
+
+    /**
+     * Return points for one pair (for the face value giving the highest points).<br/>
+     * Return 0, if there aren't 2 dice with the same face value.
+     */
+    
+
+    /**
+     * Return points for two pairs<br/>
+     * (for the 2 face values giving the highest points).<br/>
+     * Return 0, if there aren't 2 dice with the same face value<br/>
+     * and 2 other dice with the same but different face value.
+     */
+    
+
+    /**
+     * Return points for 3 of a kind.<br/>
+     * Return 0, if there aren't 3 dice with the same face value.
+     */
+    
+
+
+    /**
+     * Return points for 4 of a kind.<br/>
+     * Return 0, if there aren't 4 dice with the same face value.
+     */
+    
+
+    /**
+     * Return points for full house.<br/>
+     * Return 0, if there aren't 3 dice with the same face value<br/>
+     * and 2 other dice with the same but different face value.
+     */
+    
+
+    /**
+     * Return points for small straight.<br/>
+     * Return 0, if the dice aren't showing 1,2,3,4,5.
+     */
+    
+
+    /**
+     * Return points for large straight.<br/>
+     * Return 0, if the dice aren't showing 2,3,4,5,6.
+     */
+    
+
+    /**
+     * Return points for chance (the sum of face values).
+     */
+    
+    function chancePoints() {
+        
+    }
+
+    /**
+     * Return points for yatzy (50 points).<br/>
+     * Return 0, if there aren't 5 dice with the same face value.
+     */
