f3be9021b3768d0808b169679ffca65947bee8e9
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2024-02-29 09:24:07 +0100

    Game-logic comments for methods

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -3,3 +3,30 @@
 export default function randomName() {
     console.log("object");
 }
+
+/* Array for dice values*/
+
+// Variable for Number of times the 5 dice have been thrown.
+
+// 0 <= throwCount <= 3.
+
+// Variable for roundCount
+
+  /**
+     * Return the 5 face values of the dice.
+     */
+function getValues() {
+
+}
+
+ /**
+     * Reset the throw count.
+     */
+function resetThrowCount() {
+
+}
+
+ /**
+     * Roll the 5 dice. Only roll dice that are not hold.<br/>
+     * Note: holdStatus[index] is true, if die no. index is hold (for index in [0..4]).
+     */
